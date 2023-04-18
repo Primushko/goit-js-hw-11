51ef0640befd0d1a9df21908ec639643802b7f59
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-04-18 17:01:04 +0300

    package.json dcnfyjdktyyz

--- src/js/fetchimages.js
+++ src/js/fetchimages.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '34532945-6dd9e50d65c600f2d5972702b';
+const API_KEY = '35449241-c8ddb906a2e58eb450386dbf8';
 
 export async function fetchImages(value, page = 1, perPage = 40) {
   try {
